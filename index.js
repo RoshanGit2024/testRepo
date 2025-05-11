b40da4f666638571597e08bea7d7e2cb9bc67d2b
Anuroshan Balagiri <anuroshanb@gmail.com>
2025-05-11 22:29:09 +0530

    new last

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var a=10;
 var b=20;   
 const newFunc=()=>{
-    console.log("changed text")
+    console.log("changed text2")
 }
 const newFunc2=()=>{
-    console.log("changed text")
+    console.log("changed text2")
 }
 const newFunc3=()=>{
-    console.log("changed text")
+    console.log("changed text2")
 }
 const newFunc4=()=>{
-    console.log("changed text")
+    console.log("changed text2")
 }
